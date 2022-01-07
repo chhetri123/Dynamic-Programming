a8c12634a8c774b6658a1afaa4fc7620d3e28e82
chhetri123 <chhetridon123@gmail.com>
2022-01-07 22:26:58 +0545

    fibonacci tabulation

---
+++ tabulation/fib.js
@@ -0,0 +1,15 @@
+const fib = (n) => {
+  // it has time complexity O(n)
+  // It has space complexity O(n)
+  const table = Array(n + 1).fill(0);
+  table[1] = 1;
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table[i];
+    table[i + 2] += table[i];
+  }
+  return table[n];
+};
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
