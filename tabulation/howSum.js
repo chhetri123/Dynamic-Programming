1a37340d14225c30a648e00d01f523a0217893aa
chhetri123 <chhetridon123@gmail.com>
2022-01-10 23:20:39 +0545

    howSum tab

---
+++ tabulation/howSum.js
@@ -0,0 +1,34 @@
+// Write a function howSum( targetSum, numbers) that takes in a
+// targetSum and an array of numbers as arguments.
+
+// The function should return an array containing any combination of
+// elements that add up to exactly the targetSum.
+
+// If there is no combination that adds up to the targetSum, then
+// return null.
+
+// If there are multiple combinations possible, you may return any
+// single one.
+
+const howSum = (targetSum, numbers) => {
+  // Its time complexity isO(m^2n) where m= targetSUm and  n= numbers.length
+  // its space complexity is O(m^2)
+
+  const table = Array(targetSum + 1).fill(null);
+  table[0] = [];
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] !== null) {
+      for (let num of numbers) {
+        table[i + num] = [...table[i], num];
+      }
+    }
+  }
+  return table[targetSum];
+};
+
+console.log(howSum(6, [1, 2, 5, 6]));
+console.log(howSum(7, [2, 3])); // [3, 2, 2]
+console.log(howSum(7, [5, 3, 4, 7])); // [4,3]
+console.log(howSum(7, [2, 4])); // null
+console.log(howSum(8, [2, 3, 5])); // [2, 2, 2, 2] I
+console.log(howSum(300, [7, 14])); // null
